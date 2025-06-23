6667f778eb22bd12a9ba296334c800393b7ff62d
devxtushar <devxtushar@gmail.com>
2025-06-23 17:05:01 +0530

    # Project setup with product json data

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import './global.css';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,7 +13,6 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
     </View>
   );
 }
